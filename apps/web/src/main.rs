ed260194103d62754c367549cbf0bfcb262d3078
Develata <785967547@qq.com>
2026-01-09 16:55:53 +0800

    Optimized the rendering issues of LaTeX formulas

--- apps/web/src/main.rs
+++ apps/web/src/main.rs
@@ -9,6 +9,7 @@ use leptos::prelude::*;
 pub fn main() {
     console_error_panic_hook::set_once();
     tracing_wasm::set_as_global_default();
+    tracing::info!("Initializing Deve-Note Web App");
     
     // Hide overlay manually on mount to prevent hanging if no doc selected
     let window = web_sys::window().unwrap();
