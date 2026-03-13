69d798384e39b681874053a1ef89d0f903f10aaa
Develata <785967547@qq.com>
2026-03-13 21:08:13 +0800

    fix: avoid overlapping source control refresh requests

--- apps/web/src/hooks/use_core/effects/message_refresh.rs
+++ apps/web/src/hooks/use_core/effects/message_refresh.rs
@@ -32,9 +32,13 @@ pub fn should_send_refresh(
         && scope.branch == branch
 }
 
+pub fn can_issue_sc_refresh(pending_changes_request_id: Option<String>) -> bool {
+    pending_changes_request_id.is_none()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{RefreshScope, capture_refresh_scope, should_send_refresh};
+    use super::{RefreshScope, can_issue_sc_refresh, capture_refresh_scope, should_send_refresh};
     use crate::hooks::use_core::PendingBranchTarget;
     use deve_core::models::PeerId;
 
@@ -86,5 +90,11 @@ mod tests {
             None,
             None,
         ));
+    }
+
+    #[test]
+    fn sc_refresh_waits_for_inflight_request_to_finish() {
+        assert!(!can_issue_sc_refresh(Some("req-1".into())));
+        assert!(can_issue_sc_refresh(None));
     }
 }
