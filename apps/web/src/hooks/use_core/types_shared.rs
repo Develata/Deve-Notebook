902d9b79218973c0e3789eed4da71b227e373ce4
Develata <785967547@qq.com>
2026-03-23 18:02:54 +0800

    refactor: split web core types

---
+++ apps/web/src/hooks/use_core/types_shared.rs
@@ -0,0 +1,85 @@
+use crate::storage::DegradedSyncMode;
+use crate::storage::identity::StoredPeerIdentity;
+use deve_core::models::{DocId, PeerId, VersionVector};
+use leptos::prelude::*;
+
+use super::super::navigation::PendingNavigation;
+use super::super::pending::PendingLocalEdits;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum PendingBranchTarget {
+    Local,
+    Shadow(String),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct PeerSession {
+    pub id: PeerId,
+    pub vector: VersionVector,
+    pub last_seen: u64,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ChatMessage {
+    pub role: String,
+    pub content: String,
+    pub req_id: Option<String>,
+    pub ts_ms: u64,
+}
+
+#[derive(Clone, Copy)]
+pub struct SwitchScopeSignals {
+    pub current_doc: ReadSignal<Option<DocId>>,
+    pub pending_local_edits: ReadSignal<PendingLocalEdits>,
+    pub set_pending_navigation: WriteSignal<Option<PendingNavigation>>,
+    pub current_repo: ReadSignal<Option<String>>,
+    pub active_branch: ReadSignal<Option<PeerId>>,
+    pub pending_branch_switch: ReadSignal<Option<PendingBranchTarget>>,
+    pub pending_branch_switch_nonce: ReadSignal<Option<u64>>,
+    pub pending_repo_switch: ReadSignal<Option<String>>,
+    pub pending_repo_switch_nonce: ReadSignal<Option<u64>>,
+    pub set_handshake_ready: WriteSignal<bool>,
+    pub set_handshake_scope_nonce: WriteSignal<Option<u64>>,
+    pub set_pending_branch_switch: WriteSignal<Option<PendingBranchTarget>>,
+    pub set_pending_branch_switch_nonce: WriteSignal<Option<u64>>,
+    pub set_pending_repo_switch: WriteSignal<Option<String>>,
+    pub set_pending_repo_switch_nonce: WriteSignal<Option<u64>>,
+}
+
+#[derive(Clone, Copy)]
+pub struct HandshakeSignals {
+    pub identity: ReadSignal<Option<StoredPeerIdentity>>,
+    pub repo_vector: ReadSignal<VersionVector>,
+    pub degraded: ReadSignal<Option<DegradedSyncMode>>,
+    pub current_repo: ReadSignal<Option<String>>,
+    pub current_repo_id: ReadSignal<Option<String>>,
+    pub current_scope_nonce: ReadSignal<u64>,
+    pub active_branch: ReadSignal<Option<PeerId>>,
+    pub pending_branch_switch: ReadSignal<Option<PendingBranchTarget>>,
+    pub set_pending_branch_switch: WriteSignal<Option<PendingBranchTarget>>,
+    pub set_pending_branch_switch_nonce: WriteSignal<Option<u64>>,
+    pub pending_repo_switch: ReadSignal<Option<String>>,
+    pub set_pending_repo_switch: WriteSignal<Option<String>>,
+    pub set_pending_repo_switch_nonce: WriteSignal<Option<u64>>,
+    pub handshake_scope_nonce: ReadSignal<Option<u64>>,
+    pub set_handshake_scope_nonce: WriteSignal<Option<u64>>,
+    pub set_repo_list_request_id: WriteSignal<Option<String>>,
+    pub set_doc_list_request_id: WriteSignal<Option<String>>,
+    pub set_tree_request_id: WriteSignal<Option<String>>,
+    pub set_handshake_ready: WriteSignal<bool>,
+}
+
+#[derive(Clone, Copy)]
+pub struct RepoSwitchSignals {
+    pub current_repo: ReadSignal<Option<String>>,
+    pub current_repo_id: ReadSignal<Option<String>>,
+    pub pending_repo_switch: ReadSignal<Option<String>>,
+    pub set_pending_repo_switch: WriteSignal<Option<String>>,
+    pub pending_repo_switch_nonce: ReadSignal<Option<u64>>,
+    pub set_pending_repo_switch_nonce: WriteSignal<Option<u64>>,
+    pub current_scope_nonce: ReadSignal<u64>,
+    pub set_current_scope_nonce: WriteSignal<u64>,
+    pub set_current_repo: WriteSignal<Option<String>>,
+    pub set_current_repo_id: WriteSignal<Option<String>>,
+    pub set_current_doc: WriteSignal<Option<DocId>>,
+}
