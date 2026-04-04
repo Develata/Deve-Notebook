5aa7d2376a836edcff3860eb7a6929c7c80359bc
Develata <785967547@qq.com>
2026-04-04 19:38:25 +0800

    Remove duplicated mobile header actions

--- apps/web/src/components/mobile_layout/header.rs
+++ apps/web/src/components/mobile_layout/header.rs
@@ -56,25 +56,6 @@ pub fn MobileHeader(
                 >
                     <Terminal class="w-[18px] h-[18px]"/>
                 </button>
-                <button
-                    class=action_btn
-                    title=move || t::header::open(locale.get())
-                    aria-label=move || t::header::open(locale.get())
-                    on:click=move |_| on_open.run(())
-                >
-                    <Book class="w-[18px] h-[18px]"/>
-                </button>
-                <button
-                    class=action_btn
-                    title=move || t::header::command(locale.get())
-                    aria-label=move || t::header::command(locale.get())
-                    on:click=move |_| on_command.run(())
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <polyline points="4 17 10 11 4 5"></polyline>
-                        <line x1="12" y1="19" x2="20" y2="19"></line>
-                    </svg>
-                </button>
             </div>
         </div>
     }
