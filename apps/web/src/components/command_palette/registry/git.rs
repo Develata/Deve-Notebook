fb0c36014f8574625da1af8fb763f86b4c30d32d
Develata <785967547@qq.com>
2026-07-04 12:25:12 +0800

    refactor: split git command notices

--- apps/web/src/components/command_palette/registry/git.rs
+++ apps/web/src/components/command_palette/registry/git.rs
@@ -2,92 +2,18 @@
 //!   - 14_commands#command-palette-shortcuts
 //!   - 05_diff_logic#git-mirror-lifecycle
 //!
-use crate::components::activity_bar::SidebarView;
+mod notice;
+
+use self::notice::{show_git_status_notice, show_source_control_notice};
 use crate::components::command_palette::types::Command;
 use crate::components::main_layout::SidebarControl;
-use crate::hooks::use_core::{
-    SourceControlContext,
-    source_control_notice::{SourceControlNotice, is_local_command_notice},
-};
+use crate::hooks::use_core::{SourceControlContext, source_control_notice::SourceControlNotice};
 use crate::i18n::{Locale, t};
 use crate::runtime::session_client::SessionClient;
 use leptos::prelude::*;
 
-fn show_source_control_notice(
-    set_notice: Option<WriteSignal<Option<SourceControlNotice>>>,
-    sidebar_control: Option<SidebarControl>,
-    notice: SourceControlNotice,
-    set_show: WriteSignal<bool>,
-) {
-    if let Some(set_notice) = set_notice {
-        set_notice.set(Some(notice));
-    }
-    if let Some(sidebar_control) = sidebar_control {
-        sidebar_control.show_view(SidebarView::SourceControl);
-    }
-    set_show.set(false);
-}
-
-fn show_git_status_notice(
-    set_notice: Option<WriteSignal<Option<SourceControlNotice>>>,
-    sidebar_control: Option<SidebarControl>,
-    source_control: Option<SourceControlContext>,
-    set_show: WriteSignal<bool>,
-) {
-    show_git_status_notice_for_viewport(
-        set_notice,
-        sidebar_control,
-        source_control,
-        set_show,
-        current_command_surface_maps_to_mobile(),
-    );
-}
-
-fn show_git_status_notice_for_viewport(
-    set_notice: Option<WriteSignal<Option<SourceControlNotice>>>,
-    sidebar_control: Option<SidebarControl>,
-    source_control: Option<SourceControlContext>,
-    set_show: WriteSignal<bool>,
-    viewport_mobile: bool,
-) {
-    let is_mobile = sidebar_control
-        .as_ref()
-        .is_some_and(|sidebar_control| sidebar_control.is_mobile.get_untracked())
-        || viewport_mobile;
-    if is_mobile {
-        clear_local_command_notice(source_control.as_ref());
-        if let Some(sidebar_control) = sidebar_control {
-            sidebar_control.show_view(SidebarView::SourceControl);
-        }
-        set_show.set(false);
-        return;
-    }
-    if let Some(set_notice) = set_notice {
-        set_notice.set(Some(SourceControlNotice::git_status_cli_only()));
-    }
-    if let Some(sidebar_control) = sidebar_control {
-        sidebar_control.show_view(SidebarView::SourceControl);
-    }
-    set_show.set(false);
-}
-
-fn clear_local_command_notice(source_control: Option<&SourceControlContext>) {
-    let Some(source_control) = source_control else {
-        return;
-    };
-    if source_control
-        .notice
-        .get_untracked()
-        .as_ref()
-        .is_some_and(is_local_command_notice)
-    {
-        source_control.clear_notice.run(());
-    }
-}
-
-fn current_command_surface_maps_to_mobile() -> bool {
-    crate::components::layout_breakpoint::current_command_surface_maps_to_mobile()
-}
+#[cfg(test)]
+use self::notice::show_git_status_notice_for_viewport;
 
 fn git_bridge_enabled_when(locale: Locale) -> String {
     let mode = use_context::<SessionClient>()
